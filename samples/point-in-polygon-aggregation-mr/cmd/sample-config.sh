5570d9b6eec1ac5a03b791307324932054a91975
Michael Park <climbage@gmail.com>
2013-03-24 11:40:33 -0700

    Update sample-config.sh

--- samples/point-in-polygon-aggregation-mr/cmd/sample-config.sh
+++ samples/point-in-polygon-aggregation-mr/cmd/sample-config.sh
@@ -2,7 +2,7 @@
 
 NAME_NODE_URL=hdfs:/localhost:8020
 JOB_TRACKER_URL=localhost:8021
-SAMPLE_DIR=/user/mich7085/samples/pinp
+SAMPLE_DIR=/user/mike/samples/pinp
 
 JOB_DIR=$SAMPLE_DIR/job
 LIB_DIR=$SAMPLE_DIR/lib
