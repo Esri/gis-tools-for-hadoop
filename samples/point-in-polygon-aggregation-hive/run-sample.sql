36bb0cef4bc0371cb0ecd84f7cf4a1c3a5fb1074
Michael Park <climbage@gmail.com>
2013-03-28 14:21:06 -0700

    Update run-sample.sql

--- samples/point-in-polygon-aggregation-hive/run-sample.sql
+++ samples/point-in-polygon-aggregation-hive/run-sample.sql
@@ -1,20 +1,19 @@
 add jar
   ../lib/esri-geometry-api.jar
-  ../lib/hadoop-utilities.jar
-  ../lib/esri-hive-spatial.jar;
+  ../lib/spatial-sdk-hadoop.jar;
 
 create temporary function ST_Point as 'com.esri.hadoop.hive.ST_Point';
 create temporary function ST_Contains as 'com.esri.hadoop.hive.ST_Contains';
 
 CREATE EXTERNAL TABLE IF NOT EXISTS earthquakes (earthquake_date STRING, latitude DOUBLE, longitude DOUBLE, magnitude DOUBLE)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
-LOCATION '${env:HOME}/esri-git/spatial-tools-hadoop/applications/samples/data/earthquake-data';
+LOCATION '${env:HOME}/esri-git/gis-tools-for-hadoop/samples/data/earthquake-data';
 
 CREATE EXTERNAL TABLE IF NOT EXISTS counties (Area string, Perimeter string, State string, County string, Name string, BoundaryShape binary)                                         
 ROW FORMAT SERDE 'com.esri.hadoop.hive.serde.JsonSerde'              
-STORED AS INPUTFORMAT 'com.esri.hadoop.hive.serde.EnclosedJsonInputFormat'
+STORED AS INPUTFORMAT 'com.esri.json.hadoop.EnclosedJsonInputFormat'
 OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION '${env:HOME}/esri-git/spatial-tools-hadoop/applications/samples/data/counties-data';
+LOCATION '${env:HOME}/esri-git/gis-tools-for-hadoop/samples/data/counties-data';
 
 SELECT counties.name, count(*) cnt FROM counties
 JOIN earthquakes
