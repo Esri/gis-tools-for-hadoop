b9ea68da21c65fe1f48437163a321cc4c0472db5
Mike Park <climbage@gmail.com>
2014-09-17 15:55:10 -0700

    updated scripted and option parsing for sample

--- samples/mapreduce/src/main/java/com/esri/hadoop/examples/pointinpolygon/AggregationSampleDriver.java
+++ samples/mapreduce/src/main/java/com/esri/hadoop/examples/pointinpolygon/AggregationSampleDriver.java
@@ -1,5 +1,11 @@
 package com.esri.hadoop.examples.pointinpolygon;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -12,6 +18,7 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 public class AggregationSampleDriver
 {
+	@SuppressWarnings("static-access")
 	public static int main(String[] init_args) throws Exception {		
 		Configuration config = new Configuration();
 		
@@ -19,26 +26,33 @@ public class AggregationSampleDriver
 		// line (such as -libjars [jar files]).  What's left after .getRemainingArgs is just the arguments
 		// intended for the MapReduce job
 		String [] args = new GenericOptionsParser(config, init_args).getRemainingArgs();
+
+		Options options = new Options();
+		options.addOption(OptionBuilder.withDescription("input HDFS path to CSV points file").hasArg().isRequired().create("csv"));
+		options.addOption(OptionBuilder.withDescription("input HDFS path to JSON polygon file").hasArg().isRequired().create("json"));
+		options.addOption(OptionBuilder.withDescription("output HDFS path").hasArg().isRequired().create("out"));
 		
-		/*
-		 * Args
-		 *  [0] path to Esri JSON file
-		 *  [1] path(s) to the input data source
-		 *  [2] path to write the output of the MapReduce jobs
-		 */
-		if (args.length != 3)
-		{
-			System.out.println("Invalid Arguments");
-			print_usage();
-			throw new IllegalArgumentException();
+		GnuParser parser = new GnuParser();
+		CommandLine cmd = null;
+		
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			System.out.println(e.getMessage());
+			printUsage(options);
+			System.exit(1);
 		}
 		
-		config.set("sample.features.input", args[0]);
+		String inputCSV = cmd.getOptionValue("csv");
+		String inputJSON = cmd.getOptionValue("json");
+		String output = cmd.getOptionValue("out");
+		
+		config.set("sample.features.input", inputJSON);
 		config.set("sample.features.keyattribute", "NAME");
 		config.setInt("samples.csvdata.columns.lat", 1);
 		config.setInt("samples.csvdata.columns.long", 2);
 		
-		Job job = new Job(config);
+		Job job = Job.getInstance(config);
 
 		job.setJobName("Earthquake Data Aggregation Sample");
 
@@ -55,18 +69,17 @@ public class AggregationSampleDriver
 		job.setInputFormatClass(TextInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
 		
-		TextInputFormat.setInputPaths(job, new Path(args[1]));
-		TextOutputFormat.setOutputPath(job, new Path(args[2]));
+		TextInputFormat.setInputPaths(job, new Path(inputCSV));
+		TextOutputFormat.setOutputPath(job, new Path(output));
 
 		job.setJarByClass(AggregationSampleDriver.class);
 
 		return job.waitForCompletion(true)?  0 : 1;
 	}
 	
-	static void print_usage()
+	static void printUsage(Options options)
 	{
-		System.out.println("***");
-		System.out.println("Usage: hadoop jar aggregation-sample.jar AggregationSampleDriver -libjars [external jar references] [/hdfs/path/to]/filtergeometry.json [/hdfs/path/to]/earthquakes.csv [/hdfs/path/to/user]/output.out");
-		System.out.println("***");
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp("bin/run-sample point-in-polygon <args>", options);
 	}
 }
