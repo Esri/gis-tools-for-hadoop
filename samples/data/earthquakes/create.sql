ee59e452bf6af4f052b20718732594bc7510f6d1
Mike Park <climbage@gmail.com>
2014-09-19 08:49:04 -0700

    fix earthquakes create script

--- samples/data/earthquakes/create.sql
+++ samples/data/earthquakes/create.sql
@@ -1,3 +1,3 @@
-CREATE EXTERNAL TABLE earthquakes (earthquake_date STRING, latitude DOUBLE, longitude DOUBLE, magnitude DOUBLE)
+CREATE EXTERNAL TABLE earthquakes (earthquake_date STRING, latitude DOUBLE, longitude DOUBLE, depth double, magnitude DOUBLE)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 LOCATION '${table.location}';
